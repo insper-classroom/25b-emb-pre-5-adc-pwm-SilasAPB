4d590cd0930c6021edbd04ed660a7cb4f4f50263
Silas Pereira <silasapb@al.insper.edu.br>
2025-09-22 15:26:26 -0300

    ex1_1

--- exe1/main.c
+++ exe1/main.c
@@ -10,21 +10,26 @@
 #include "hardware/adc.h"
 
 void adc_task(void *p) {
-    adc_init();
     adc_gpio_init(27);
+    adc_gpio_init(26);
 
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
 
-    uint16_t result;
+    uint16_t result0;
+    uint16_t result1;
     while (1) {
         adc_select_input(1); // Select ADC input 1 (GPIO27)
-        result = adc_read();
-        printf("voltage 1: %f V\n", result * conversion_factor);
+        result1 = adc_read();
+        printf("voltage 1: %f V\n", result1 * conversion_factor);
+
+        vTaskDelay(pdMS_TO_TICKS(200));
+
+        adc_select_input(0); // Select ADC input 1 (GPIO27)
+        result0 = adc_read();
+        printf("voltage 2: %f V\n", result0 * conversion_factor);
 
         // CÓDIGO AQUI
-
-
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
@@ -33,6 +38,7 @@ void adc_task(void *p) {
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
+
     adc_init();
 
     xTaskCreate(adc_task, "ADC_Task", 4095, NULL, 1, NULL);
