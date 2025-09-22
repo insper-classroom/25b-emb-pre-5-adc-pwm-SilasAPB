ab6127053d0a4eecb76893b519d57733dad75946
Silas Pereira <silasapb@al.insper.edu.br>
2025-09-22 16:01:40 -0300

    ex1_3

--- exe1/main.c
+++ exe1/main.c
@@ -9,23 +9,22 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
-uint16_t result0;
-uint16_t result1;
 void adc_task(void *p) {
     adc_gpio_init(27);
     adc_gpio_init(26);
 
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
+
     while (1) {
         adc_select_input(1); // Select ADC input 1 (GPIO27)
-        result1 = adc_read();
+        uint16_t result1 = adc_read();
         printf("voltage 1: %f V\n", result1 * conversion_factor);
 
         vTaskDelay(pdMS_TO_TICKS(200));
 
         adc_select_input(0); // Select ADC input 1 (GPIO27)
-        result0 = adc_read();
+        uint16_t result0 = adc_read();
         printf("voltage 2: %f V\n", result0 * conversion_factor);
 
         // CÓDIGO AQUI
